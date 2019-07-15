f8ef9b3aa8030b7646a6dd2e900e098334c317a6
Rudy <RodriguezLucha@gmail.com>
2019-07-15 08:50:42 -0700

    clean up

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className={styles.blue}>1</div>
+      <div className={styles.green}>2</div>
+    </div>
+  );
+}
+
+export default App;
