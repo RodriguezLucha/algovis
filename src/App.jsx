49f1be22955b8a924b222e3c62896bb0bbca65bd
Rudy Rodriguez Lucha <rodriguezlucha@gmail.com>
2019-12-24 15:40:22 -0800

    Apply opacity animation on click of button

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import styles from './App.module.scss';
-import { CSSTransitionGroup } from 'react-transition-group'
+const classNames = require('classnames');
 
 class App extends React.Component {
 
   state = {
-    boxes: ['blue', 'green']
+    boxes: ['blue', 'green'],
+    toggled: true
   }
 
   swap(){
-    this.setState({boxes: this.state.boxes.reverse()});
+    this.setState({
+      toggled: !this.state.toggled
+    });
   }
 
   render(){
     return (
       <div className={styles.app}>
-        {this.state.boxes.map(box => <div className={styles[box]} key={box}>{box}</div>)}
+        {this.state.boxes.map(box => <div className={classNames(styles[box], this.state.toggled ? styles['toggled']: '')} key={box}>{box}</div>)}
         <button className={styles.button} onClick={() => this.swap()}>Swap</button>
       </div>
     );
