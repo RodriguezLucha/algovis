a4dbe04b1910ea807df81c8fcc6959924318bcca
Rudy Rodriguez Lucha <rodriguezlucha@gmail.com>
2019-12-24 16:31:40 -0800

    Smoothly swap places of boxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
 import React from 'react';
 import styles from './App.module.scss';
+import './App.css';
+
 const classNames = require('classnames');
 
 class App extends React.Component {
 
   state = {
-    boxes: ['blue', 'green'],
+    boxpos: ['pos1', 'pos2'],
     toggled: true
   }
 
   swap(){
     this.setState({
-      toggled: !this.state.toggled
+      toggled: !this.state.toggled,
+      boxpos: this.state.boxpos.reverse(),
     });
   }
 
   render(){
     return (
       <div className={styles.app}>
-        {this.state.boxes.map(box => <div className={classNames(styles[box], this.state.toggled ? styles['toggled']: '')} key={box}>{box}</div>)}
         <button className={styles.button} onClick={() => this.swap()}>Swap</button>
+        <div className="box-container">
+          <div className={classNames('box1', this.state.boxpos[0])}>Box 1</div>
+          <div className={classNames('box2', this.state.boxpos[1])}>Box 2</div>
+        </div>
+
       </div>
     );
   }
