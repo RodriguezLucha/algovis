b17983d8ec893fcb956c758e85b0301bcf8b618f
Rudy Rodriguez Lucha <rodriguezlucha@gmail.com>
2019-12-27 15:13:30 -0800

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,32 @@
 import React from 'react';
-import styles from './App.module.scss';
+// import styles from './App.module.scss';
 import './App.css';
 
-const classNames = require('classnames');
+// const classNames = require('classnames');
 
 class App extends React.Component {
 
-  state = {
-    boxpos: ['pos1', 'pos2'],
-    toggled: true
-  }
+  // state = {
+  //   boxpos: ['pos1', 'pos2'],
+  //   toggled: true
+  // }
 
-  swap(){
-    this.setState({
-      toggled: !this.state.toggled,
-      boxpos: this.state.boxpos.reverse(),
-    });
-  }
+  // swap(){
+  //   this.setState({
+  //     toggled: !this.state.toggled,
+  //     boxpos: this.state.boxpos.reverse(),
+  //   });
+  // }
 
   render(){
     return (
-      <div className={styles.app}>
-        <button className={styles.button} onClick={() => this.swap()}>Swap</button>
+      <div>
+        {/* <button className={styles.button} onClick={() => this.swap()}>Swap</button>
         <div className="box-container">
           <div className={classNames('box1', this.state.boxpos[0])}>Box 1</div>
           <div className={classNames('box2', this.state.boxpos[1])}>Box 2</div>
-        </div>
+        </div> */}
+        ...
 
       </div>
     );
